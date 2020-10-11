2207eddc2a3d9bdf6089e8b1e19dc5c478ade455
Alfonso Betancourt <alfonso@devtence.com>
2020-10-11 00:58:13 -0300

    adding middleware function example

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,16 @@
 
 module.exports = {
   restApi: (app) => {
+    //middleware function 
+    app.use((req, res, next) => {
+      // do something
+      console.log('middleware function')
+      next();
+    });
+
+    // example route api/mobile/v1/analytics
     app.get('/status', (req, res) =>  {
       res.status(200).send({"status": "service running!"})
-    });
+    }); 
   }
 }
